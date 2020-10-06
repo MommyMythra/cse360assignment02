9d23bbee494cb64b9f70e606c7046b19afd68b61
Mommy Mythra <jkelkins@asu.edu>
2020-10-05 21:31:51 -0700

    Updated to remove main method

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -11,45 +11,49 @@ public class AddingMachine {
      * Constructor that instantiates any AddingMachine object
      * Sets total to base 0 and history to output "0"
      */
-    public AddingMachine () {
+    public AddingMachine() {
         total = 0;  // not needed - included for clarity
         history = "0";
     }
 
     /**
      * This method returns the total of the program
+     *
      * @return the total of the program
      */
-    public int getTotal () {
+    public int getTotal() {
         return total;
     }
 
     /**
      * Adds the passed value to the total.
      * Also updates the history string to keep the memory up to date
+     *
      * @param value added to the total
      */
-    public void add (int value) {
-        total = total+value;
-        history=history+" + "+value;
+    public void add(int value) {
+        total = total + value;
+        history = history + " + " + value;
     }
 
     /**
      * Subtracts the passed value from the total.
      * Also updates the history string to keep the memory up to date
+     *
      * @param value being subtracted from the total
      */
-    public void subtract (int value) {
-        total = total-value;
-        history=history+" - "+value;
+    public void subtract(int value) {
+        total = total - value;
+        history = history + " - " + value;
     }
 
     /**
      * This tracks and then returns every transaction committed by the program
      * These transactions are the "memory" of the program.
+     *
      * @return a string that represents the history of the transactions
      */
-    public String toString () {
+    public String toString() {
         return history;
     }
 
@@ -57,16 +61,7 @@ public class AddingMachine {
      * This empties out the "memory" of the program by setting total back to 0 and setting history back to "0"
      */
     public void clear() {
-        total=0;
-        history="0";
-    }
-
-    public static void main(String[] args){
-        AddingMachine myCalculator = new AddingMachine();
-        myCalculator.add (4);
-        myCalculator.subtract (2);
-        myCalculator.add(5);
-        System.out.println(myCalculator.toString());
-        System.out.println(myCalculator.getTotal());
+        total = 0;
+        history = "0";
     }
 }
